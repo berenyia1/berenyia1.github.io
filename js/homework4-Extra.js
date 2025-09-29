1973ed67ed7c853ac653843906757085aac6b14c
Antal Berenyi <aberenyi@gmail.com>
2025-09-29 22:06:58 +0200

    Extra Credit.

--- js/homework4-Extra.js
+++ js/homework4-Extra.js
@@ -1,3 +1,57 @@
 // JavaScript source code for Homework 4, Extra
 
-alert("WH4-Extra: JavaScript is running!"); 
+function formSubmitted(e) {
+    e.preventDefault();
+
+    let size = document.getElementById("size").value;
+
+    size = parseInt(size);
+
+    if (isNaN(size) || size < 2 || size > 10) {
+        showOutput("Please enter a valid size between 2 and 10.");
+        return;
+    }
+    showOutput("");
+    const output = generateSquare(size);
+    showOutput(output);
+
+}
+
+function topBottomRow(size) {
+    let row = "<div>*";
+    for (let j = 1; j < size; j++)
+        row += " *";
+    row += "</div>";
+    return row;
+}
+
+function innerRow(size) {
+    let row = `<div>*<span class="transparent">`;
+
+    for (let j = 2; j < size; j++)
+        row += " *";
+    row += " </span>*</div>";
+    return row;
+}
+
+
+// generate the square
+// <div>* * * * *</div>
+// <div>*<span class="transparent"> * * * </span>*</div>
+// <div>*<span class="transparent"> * * * </span>*</div>
+// <div>*<span class="transparent"> * * * </span>*</div>
+// <div>* * * * *</div>
+function generateSquare(size) {
+    let square = topBottomRow(size);
+
+    // rows except top and bottom
+    for (let i = 0; i < size-2; i++) {
+        square += innerRow(size);
+    }
+    square += topBottomRow(size);
+    return square;
+}
+
+function showOutput(output) {
+    document.getElementById("output").innerHTML = output;
+}

7e325872c65b241fe65877e3bf70c83d59ba6bc1
Antal Berenyi <aberenyi@gmail.com>
2025-09-29 20:32:54 +0200

    Homework Part One

--- js/homework4-001.js
+++ js/homework4-001.js
@@ -1,3 +1,58 @@
 // JavaScript source code for Homework 4, Exercise 1
 
-alert("WH4-001: JavaScript is running!"); 
+
+let tmp = 0;
+let outputforSum = document.getElementById("homework4-001-forSum");
+outputforSum.innerHTML = "The result of ";
+for (let i = 5; i <= 25; i += 4) {
+    outputforSum.innerHTML += i;
+    if (i < 25) {
+        outputforSum.innerHTML += " + ";
+    }
+
+    tmp += i;
+}
+outputforSum.innerHTML += " is " + tmp + ".";
+
+tmp = 1;
+let outputforProduct = document.getElementById("homework4-001-forProduct");
+outputforProduct.innerHTML = "The result of ";
+for (let i = 5; i <= 25; i += 4) {
+    outputforProduct.innerHTML += i;
+    if (i < 25) {
+        outputforProduct.innerHTML += " * ";
+    }
+
+    tmp *= i;
+}
+outputforProduct.innerHTML += " is " + tmp + ".";
+
+let outputwhileSum = document.getElementById("homework4-001-whileSum");
+tmp = 0;
+outputwhileSum.innerHTML = "The result of ";
+let i = 5;
+while (i <= 25) {
+    outputwhileSum.innerHTML += i;
+    if (i < 25) {
+        outputwhileSum.innerHTML += " + ";
+    }    
+    tmp += i;
+    i += 4;
+}
+outputwhileSum.innerHTML += " is " + tmp + ".";
+
+
+tmp = 1;
+i = 5;
+let outputwhileProduct = document.getElementById("homework4-001-whileProduct");
+outputwhileProduct.innerHTML = "The result of ";
+while (i <= 25) {
+    outputwhileProduct.innerHTML += i;
+    if (i < 25) {
+        outputwhileProduct.innerHTML += " * ";
+    }    
+    tmp *= i;
+    i += 4;
+}
+outputwhileProduct.innerHTML += " is " + tmp + ".";
+
